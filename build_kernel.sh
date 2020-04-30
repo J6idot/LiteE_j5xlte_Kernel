c5acce09bcc6239cf5cb745ed6480cc3d26d4899
J6idot <sesasoh@gmail.com>
2020-04-30 13:03:19 +0200

    LiteE_j5xnlte_Kernel

---
+++ build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/home/j6idot/LiteE_Kernels/arm-eabi-4.8/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
