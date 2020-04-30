61e010eced69c110de1aea3a3a036d42405a8788
J6idot <sesasoh@gmail.com>
2020-04-30 15:39:15 +0200

    Updated kernel script

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,6 +2,7 @@
 
 export ARCH=arm
 export CROSS_COMPILE=/home/j6idot/LiteE_Kernels/arm-eabi-4.8/bin/arm-eabi-
+export LOCALVERSION=-LiteE_J52016_Kernel-30042020
 mkdir output
 
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
